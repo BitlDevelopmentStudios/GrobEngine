305a8deedadaa3256b41e5d9043ad7a1afa2e6fd
Bitl <bitlblox@gmail.com>
2021-12-02 09:05:15 -0700

    First Commit

---
+++ GrobEngine/FNA-App/GrobEngine.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace GrobEngine
+{
+    public class GrobEngineMain : Game
+    {
+        static void Main(string[] args)
+        {
+            // begin engine loop
+            using (GrobEngineMain g = new GrobEngineMain())
+            {
+                g.Run();
+            }
+        }
+
+        //the main engine script.
+        private IGame mainScript;
+        public GraphicsDeviceManager graphics;
+
+        private GrobEngineMain() : base()
+        {
+            //set the content root directory.
+            Content.RootDirectory = "Data";
+
+            //load main engine script.
+            IGame tempLoader = ScriptEngine.LoadScriptFromContent("Engine/Scripts/EngineMain.cs", Content);
+
+            if (tempLoader != null)
+            {
+                mainScript = tempLoader;
+            }
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.DeviceCreated += OnDeviceCreated;
+            graphics.DeviceReset += OnDeviceReset;
+        }
+
+        protected override void Initialize()
+        {
+            mainScript.Initialize(this);
+            base.Initialize();
+        }
+
+        private void OnDeviceCreated(object sender, EventArgs e)
+        {
+            mainScript.OnDeviceCreated(this, sender, e);
+        }
+
+        private void OnDeviceReset(object sender, EventArgs e)
+        {
+            mainScript.OnDeviceReset(this, sender, e);
+        }
+
+        protected override void LoadContent()
+        {
+            mainScript.LoadContent(this);
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            mainScript.UnloadContent(this);
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            mainScript.Update(this, gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            mainScript.Draw(this, gameTime);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GrobEngine/FNA-App/ScriptEngine.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.IO;
+using System.Linq;
+
+// based on https://stackoverflow.com/questions/137933/what-is-the-best-scripting-language-to-embed-in-a-c-sharp-desktop-application
+namespace GrobEngine
+{
+    public interface IGame
+    {
+        void Initialize(GrobEngineMain game);
+        void OnDeviceCreated(GrobEngineMain game, object sender, EventArgs e);
+        void OnDeviceReset(GrobEngineMain game, object sender, EventArgs e);
+        void LoadContent(GrobEngineMain game);
+        void UnloadContent(GrobEngineMain game);
+        void Update(GrobEngineMain game, GameTime gameTime);
+        void Draw(GrobEngineMain game, GameTime gameTime);
+     }
+
+    public class ScriptEngine
+    {
+        public static IGame LoadScriptFromContent(string scriptpath, ContentManager content)
+        {
+            string fullScriptPath = content.RootDirectory + '/' + scriptpath;
+
+            try
+            {
+                using (var stream = TitleContainer.OpenStream(fullScriptPath))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string script = reader.ReadToEnd();
+                        Assembly compiled = CompileScript(script, fullScriptPath);
+                        IGame code = ExecuteScript(compiled);
+                        return code;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler(fullScriptPath + ": " + ex.Message, true);
+            }
+
+            return null;
+        }
+
+        private static IGame ExecuteScript(Assembly assemblyScript)
+        {
+            if (assemblyScript == null)
+            {
+                goto error;
+            }
+
+            foreach (Type type in assemblyScript.GetExportedTypes())
+            {
+                foreach (Type intface in type.GetInterfaces())
+                {
+                    if (intface == typeof(IGame))
+                    {
+                        ConstructorInfo constructor = type.GetConstructor(Type.EmptyTypes);
+
+                        if (constructor != null && constructor.IsPublic)
+                        {
+                            IGame finalScript = constructor.Invoke(null) as IGame;
+                            return finalScript;
+                        }
+                        else
+                        {
+                            ErrorHandler("Constructor does not exist or it is not public.", true);
+                            return null;
+                        }
+                    }
+                }
+            }
+
+error:
+            ErrorHandler("Failed to load script.", true);
+            return null;
+        }
+
+        private static Assembly CompileScript(string code, string filePath)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+
+            CompilerParameters perams = new CompilerParameters();
+            perams.GenerateExecutable = false;
+            perams.GenerateInMemory = true;
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic).Select(a => a.Location);
+            perams.ReferencedAssemblies.AddRange(assemblies.ToArray());
+
+            CompilerResults result = provider.CompileAssemblyFromSource(perams, code);
+
+            foreach (CompilerError error in result.Errors)
+            {
+                ErrorHandler(error, filePath, error.IsWarning, false);
+            }
+
+            if (result.Errors.HasErrors)
+            {
+                return null;
+            }
+
+            return result.CompiledAssembly;
+        }
+
+        private static void ErrorHandler(string error, bool finalError)
+        {
+            ErrorHandler(error, false, finalError);
+        }
+
+        private static void ErrorHandler(string error, bool warning, bool finalError)
+        {
+            Console.WriteLine(warning ? "[WARNING] - " : "[ERROR] - " 
+                + error);
+            PauseAndExit(finalError);
+        }
+
+        private static void ErrorHandler(CompilerError error, string fileName, bool warning, bool finalError)
+        {
+            Console.WriteLine(warning ? "[WARNING] - " : "[ERROR] - " +
+                fileName + " (" + error.Line + "," + error.Column + "): " 
+                + error.ErrorText);
+            PauseAndExit(finalError);
+        }
+
+        private static void PauseAndExit(bool pause)
+        {
+            if (pause)
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+    }
+}
